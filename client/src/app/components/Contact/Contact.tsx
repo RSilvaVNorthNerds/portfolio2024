dfbad173d18f7ff85c1cbd2c4fc2423e897bd25e
Rafael Silva <rafaelsilva@northnerds.com>
2024-08-18 12:34:22 -0700

    Basic contact section skeleton

--- client/src/app/components/Contact/Contact.tsx
+++ client/src/app/components/Contact/Contact.tsx
@@ -1,3 +1,13 @@
+import { Box, Typography } from "@mui/material";
+import ContactForm from "./components/ContactForm";
+
 export default function Contact() {
-  return <></>;
+  return (
+    <Box>
+      <Typography variant="h3">Contact:</Typography>
+      <Box sx={{ display: "flex" }}>
+        <ContactForm />
+      </Box>
+    </Box>
+  );
 }
--- client/src/app/components/ProjectShowcase/ProjectShowcase.tsx
+++ client/src/app/components/ProjectShowcase/ProjectShowcase.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "./components/ProjectCard";
 export default function ProjectShowcase() {
   return (
     <Box>
-      <Typography variant="h6">Projects:</Typography>
+      <Typography variant="h3">Projects:</Typography>
       <Box sx={{ display: "flex" }}>
         <ProjectCard name="Project 1" />
       </Box>
