bfeea166ecd6842d6c8a879a1384c2d7e048c478
Rafael Silva <rafaelsilva@northnerds.com>
2024-10-16 11:54:10 -0700

    remove bloat files

--- client2/src/App.tsx
+++ client2/src/App.tsx
@@ -1,19 +1,26 @@
-import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Banner from "./components/Banner/Banner";
 import CoreSkillCarousel from "./components/CoreSkillCarousel/CoreSkillCarousel";
 import ProjectShowcase from "./components/ProjectShowcase/ProjectShowcase";
 import Contact from "./components/Contact/Contact";
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <>
+    <Box
+      sx={{
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
       <Navigation />
       <Banner />
       <CoreSkillCarousel />
       <ProjectShowcase />
       <Contact />
-    </>
+    </Box>
   );
 }
 
